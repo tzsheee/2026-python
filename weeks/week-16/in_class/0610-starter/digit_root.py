cf59ec6bb7f79d2d0dc6315ea376eae5c4784198
student <student@example.com>
2026-06-10 14:03:15 +0800

    feat: implement digit root

--- weeks/week-16/in_class/0610-starter/digit_root.py
+++ weeks/week-16/in_class/0610-starter/digit_root.py
@@ -8,6 +8,7 @@ def digit_root(n: int) -> int:
         raise TypeError("n must be an int")
     if n < 1:
         raise ValueError("n must be >= 1")
+    # Use the congruence formula for digital root for efficiency
     if n < 10:
         return n
     return 1 + (n - 1) % 9
